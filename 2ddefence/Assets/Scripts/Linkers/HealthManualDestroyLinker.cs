590065e8a9bb0cf4875a28d7b99daf7b0c6751fd
bhsohn <hanmuson1@gmail.com>
2025-10-16 17:27:00 +0900

    RequireComponent 관련 처리

--- 2ddefence/Assets/Scripts/Linkers/HealthManualDestroyLinker.cs
+++ 2ddefence/Assets/Scripts/Linkers/HealthManualDestroyLinker.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 using R3;
 
+[RequireComponent(typeof(HealthComponent), typeof(ManualDestroyComponent))]
 public class HealthManualDestroyLinker : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -9,10 +10,16 @@ public class HealthManualDestroyLinker : MonoBehaviour
         var health = GetComponent<HealthComponent>();
         var manualDestoy = GetComponent<ManualDestroyComponent>();
 
+        if (health == null || manualDestoy == null)
+        {
+            Debug.LogError("not found Component.");
+            return;
+        }
+
         // health.DeathSubject.OnNext가 실행되는 아래 함수 실행
         health.OnDeath
-            .Subscribe(_ => manualDestoy.DestroySelf())
-            .AddTo(this); // 리소스 낭비 방지를 위해 추가
+        .Subscribe(_ => manualDestoy.DestroySelf())
+        .AddTo(this); // 리소스 낭비 방지를 위해 추가
     }
 
     // Update is called once per frame
