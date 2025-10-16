d12cccf6d5b9f4fefd31e795ebe355a4a46b0752
bhsohn <hanmuson1@gmail.com>
2025-10-17 06:37:38 +0900

    방어 타워 관련 작업

--- 2ddefence/Assets/Scripts/Components/AttackControllerComponent.cs
+++ 2ddefence/Assets/Scripts/Components/AttackControllerComponent.cs
@@ -1,16 +1,40 @@
+using R3;
 using UnityEngine;
 
 public class AttackControllerComponent : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private readonly Subject<Unit> _attackStartSubject = new Subject<Unit>();
+    private readonly Subject<Unit> _attackEndSubject = new Subject<Unit>();
+    public Observable<Unit> OnAttackStart => _attackStartSubject;
+    public Observable<Unit> OnAttackEnd => _attackEndSubject;
+
+    private bool _isAttacking = false;
+
+    private void Awake()
     {
-        
+        // 메모리 보호용 
+        _attackStartSubject.AddTo(this);
+        _attackEndSubject.AddTo(this);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void StartAttack()
     {
-        
+        if (!_isAttacking)
+        {
+            Debug.Log("Start Attack");
+            _isAttacking = true;
+            _attackStartSubject.OnNext(Unit.Default);
+        }
+
+
+    }
+    public void EndAttack()
+    {
+        if (_isAttacking)
+        {
+            Debug.Log("End Attack");
+            _isAttacking = false;
+            _attackEndSubject.OnNext(Unit.Default);
+        }
     }
 }
