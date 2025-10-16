b39ae3ddbfb0be5ec372e02174cc22c3dff70ffa
bhsohn <hanmuson1@gmail.com>
2025-10-16 16:35:41 +0900

    2d defence 처리

--- 2ddefence/Assets/Scripts/Components/HealthComponent.cs
+++ 2ddefence/Assets/Scripts/Components/HealthComponent.cs
@@ -4,13 +4,39 @@ public class HealthComponent : MonoBehaviour, IDamageable
 {
     public int MaxHealth = 100;
     public int CurrentHealth;
+
+    private bool _isAlive = true;
+    public bool IsAlive => _isAlive;
+
     private void Awake()
     {
         CurrentHealth = MaxHealth;
+        _isAlive = true;
     }
 
     public void TakeDamage(int damage)
     {
+        if (!_isAlive)
+        {
+            return;
+        }
+
         CurrentHealth -= damage;
+        if (CurrentHealth <= 0)
+        {
+            Die();
+        }
+
+    }
+    public void Die()
+    {
+        if (!_isAlive)
+        {
+            return;
+        }
+
+        _isAlive = false;
+
+        Debug.Log("Die");
     }
 }
--- 2ddefence/Assets/Scripts/Managers/IDamageable.cs
+++ 2ddefence/Assets/Scripts/Managers/IDamageable.cs
@@ -3,4 +3,5 @@ using UnityEngine;
 public interface IDamageable
 {
     void TakeDamage(int damage);
+    bool IsAlive { get; }
 }
