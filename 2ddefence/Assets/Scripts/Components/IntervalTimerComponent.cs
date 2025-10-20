073f3fba24f60bfdb15cd5c81320359dd7ecc4b0
bhsohn <hanmuson1@gmail.com>
2025-10-20 10:19:57 +0900

    2d timer 관련 처리

--- 2ddefence/Assets/Scripts/Components/IntervalTimerComponent.cs
+++ 2ddefence/Assets/Scripts/Components/IntervalTimerComponent.cs
@@ -5,6 +5,8 @@ public class IntervalTimerComponent : MonoBehaviour
 {
     [SerializeField] float _interval = 1.0f;
     private float _timer = 0f;
+    private bool _isActive = false;
+
     private readonly Subject<Unit> _intervalElapsedSubject = new Subject<Unit>();
     public Observable<Unit> OnIntervalElapsed => _intervalElapsedSubject;
     private void Awake()
@@ -15,6 +17,9 @@ public class IntervalTimerComponent : MonoBehaviour
 
     void Update()
     {
+        if (!_isActive)
+            return;
+
         _timer += Time.deltaTime;
         if (_timer >= _interval)
         {
@@ -22,5 +27,14 @@ public class IntervalTimerComponent : MonoBehaviour
             Debug.Log($"IntervalTimer Event {_interval}second");
             _timer = 0.0f;
         }
+    }
+    public void Active()
+    {
+        _isActive = true;
+        _timer = _interval;
+    }
+    public void DeActive()
+    {
+        _isActive = false;
     }
 }
